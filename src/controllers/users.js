621471dba97ffff4a950399a7ad99376074e2fba
Mark <xenicmark@gmail.com>
2022-09-06 01:05:06 +0300

    implemented user profile feature

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,7 +6,11 @@ class userController {
     const { userName, email, password, firstName, lastName } = req.body;
     const emailExists = await User.findOne({ email });
     if (emailExists)
-      return res.status(400).json({ message: 'User already registered' });
+      return res.status(400).json({ message: 'Email already in use' });
+
+    const usernameExists = await User.findOne({ userName });
+    if (usernameExists)
+      return res.status(400).json({ message: 'Username already in use' });
 
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(password, salt);
@@ -26,6 +30,63 @@ class userController {
       'Last Name': lastName,
     });
   }
-}
 
+  static async getuserProfile(req, res) {
+    const user = await User.findOne({ userName: req.params.username });
+    if (!user) return res.status(400).json({ message: 'User not found' });
+    return res.status(200).json({
+      _id: String(user._id),
+      userName: user.userName,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      socialMedia: user.socialMedia,
+      tel: user.tel,
+      bio: user.bio,
+      website: user.website,
+    });
+  }
+
+  static async updateUserProfile(req, res) {
+    const user = await User.findOne({ userName: req.params.username });
+
+    if (String(user._id) !== req.user.userId)
+      return res.status(401).json({ message: 'Not authorized' });
+
+    const { email, firstName, lastName, socialMedia, tel, bio, website } =
+      req.body;
+
+    const updatedProfile = await User.findByIdAndUpdate(
+      { _id: String(user._id) },
+      {
+        $set: {
+          email,
+          firstName,
+          lastName,
+          socialMedia,
+          tel,
+          bio,
+          website,
+        },
+      },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      _id: updatedProfile._id,
+      userName: updatedProfile.userName,
+      email: updatedProfile.email,
+      firstName: updatedProfile.firstName,
+      lastName: updatedProfile.lastName,
+      socialMedia: updatedProfile.socialMedia,
+      tel: updatedProfile.tel,
+      bio: updatedProfile.bio,
+      website: updatedProfile.website,
+    });
+  }
+}
+/**
+ * TODO: Figure out how to check whether the new updated email is not already in use
+ * TODO: Add a way to update the social media array
+ */
 export default userController;
--- src/models/users.js
+++ src/models/users.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  userName: { type: String, min: 5, max: 50 },
+  userName: { type: String, minLength: 5, maxLength: 50, lowercase: true },
   email: { type: String, unique: true },
   password: String,
-  firstName: { type: String, min: 2, max: 50 },
-  lastName: { type: String, min: 2, max: 50 },
+  firstName: { type: String, minLength: 2, maxLength: 50 },
+  lastName: { type: String, minLength: 2, maxLength: 50 },
   socialMedia: [String],
   website: { type: String },
   role: {
@@ -13,6 +13,8 @@ const userSchema = new mongoose.Schema({
     default: 'user',
     enum: ['admin', 'user'],
   },
+  bio: { type: String, minLength: 1, maxLength: 5000 },
+  tel: { type: String, minLength: 4 },
 });
 
 export default mongoose.model('User', userSchema);
